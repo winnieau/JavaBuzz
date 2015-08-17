b5ff25c325f9ef178ec63633ad3bdc0babc59601
Winifred Au <winifredau1990@gmail.com>
2015-08-17 06:36:36 -0400

    first commit

---
+++ src/Javabuzz.js
@@ -0,0 +1,31 @@
+// function Player() {
+// }
+// Player.prototype.play = function(song) {
+//   this.currentlyPlayingSong = song;
+//   this.isPlaying = true;
+// };
+//
+// Player.prototype.pause = function() {
+//   this.isPlaying = false;
+// };
+//
+// Player.prototype.resume = function() {
+//   if (this.isPlaying) {
+//     throw new Error("song is already playing");
+//   }
+//
+//   this.isPlaying = true;
+// };
+//
+// Player.prototype.makeFavorite = function() {
+//   this.currentlyPlayingSong.persistFavoriteStatus(true);
+// };
+function Game() {
+}
+Game.prototype.isDivisibleByThree = function(number) {
+  if (number % 3 === 0) {
+    return true;
+  } else {
+    return false;
+    }
+};
